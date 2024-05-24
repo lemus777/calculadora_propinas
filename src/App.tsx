47c8101aac42a4b70d720bcd6ff16c82a1539b3c
lemus777 <lemus777@gmail.com>
2024-05-24 13:44:00 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import MenuItem from "./components/MenuItem"
+import { menuItems } from "./data/db"
+
+function App() {
+  
+  return (
+    <>
+      <header className=" bg-teal-400 py-5 ">
+        <h1 className=" text-center text-4xl font-black ">Calculadora de propinas y consumo</h1>
+      </header>
+
+      <main className=" max-w-7xl mx-auto py-20 grid md:grid-cols-2 ">
+        <div>
+          <h2>Menú</h2>
+          {menuItems.map(item => (
+            <MenuItem
+              key={item.id}
+            />
+          ))}
+        </div>
+        <div>
+          <h2>Consumo</h2>
+        </div>
+      </main>
+    </>
+  )
+}
+
+export default App
