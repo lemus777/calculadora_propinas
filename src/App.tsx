6bb1a34d5f76ffa1407f3489257717ae4caae316
lemus777 <lemus777@gmail.com>
2024-05-27 14:22:20 +0200

    product list and subtotal implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import MenuItem from "./components/MenuItem"
+import OrderContents from "./components/OrderContents"
+import OrderTotals from "./components/OrderTotals"
+import TipPercentageForm from "./components/TipPercentageForm"
 import { menuItems } from "./data/db"
+import useOrder from "./hooks/useOrder"
 
 function App() {
+
+  const { order, addItem, removeItem } = useOrder()
   
   return (
     <>
@@ -10,16 +16,27 @@ function App() {
       </header>
 
       <main className=" max-w-7xl mx-auto py-20 grid md:grid-cols-2 ">
-        <div>
-          <h2>Menú</h2>
-          {menuItems.map(item => (
-            <MenuItem
-              key={item.id}
-            />
-          ))}
+        <div className="p-5">
+          <h2 className="text-4xl font-black">Menú</h2>
+          <div className="space-y-3 mt-10">
+            {menuItems.map(item => (
+              <MenuItem
+                key={item.id}
+                item={item}
+                addItem={addItem}
+              />
+            ))}
+          </div>
         </div>
-        <div>
-          <h2>Consumo</h2>
+        <div className="border border-dashed border-slate-300 p-5 rounded-lg space-y-10">
+          <OrderContents
+            order={order}
+            removeItem={removeItem}
+          />
+          <TipPercentageForm />
+          <OrderTotals
+            order={order}
+          />
         </div>
       </main>
     </>
--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -1,5 +1,18 @@
-export default function MenuItem() {
+import type { MenuItem } from "../types"
+
+type MenuItemProps = {
+  item: MenuItem
+  addItem: (item : MenuItem) => void
+}
+
+export default function MenuItem({ item, addItem } : MenuItemProps) {
   return (
-    <div>MenuItem</div>
+    <button
+      className="border-2 rounded-lg border-teal-400 hover:bg-teal-200 w-full p-3 flex justify-between"
+      onClick={() => addItem(item)}
+    >
+      <p>{item.name}</p>
+      <p className="font-black">${item.price}</p>
+    </button>
   )
 }
