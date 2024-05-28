8d90bd38ecd1ee1ec8467ddb927e254aa59ab3c7
lemus777 <lemus777@gmail.com>
2024-05-28 11:47:40 +0200

    calculadora propinas finished

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import useOrder from "./hooks/useOrder"
 
 function App() {
 
-  const { order, addItem, removeItem } = useOrder()
+  const { order, tip, setTip, addItem, removeItem, placeOrder } = useOrder()
   
   return (
     <>
@@ -29,14 +29,25 @@ function App() {
           </div>
         </div>
         <div className="border border-dashed border-slate-300 p-5 rounded-lg space-y-10">
-          <OrderContents
-            order={order}
-            removeItem={removeItem}
-          />
-          <TipPercentageForm />
-          <OrderTotals
-            order={order}
-          />
+          { order.length ? (
+            <>
+              <OrderContents
+                order={order}
+                removeItem={removeItem}
+              />
+              <TipPercentageForm
+                setTip={setTip}
+                tip={tip}
+              />
+              <OrderTotals
+                order={order}
+                tip={tip}
+                placeOrder={placeOrder}
+              />
+            </>
+          ) : (
+            <p className="text-center">La orden está vacía</p>
+          )}
         </div>
       </main>
     </>
--- src/components/OrderContents.tsx
+++ src/components/OrderContents.tsx
@@ -11,10 +11,7 @@ export default function OrderContents({order, removeItem} : OrderContentsProps) 
     <div>
       <h2 className="font-black text-4xl">Consumo</h2>
       <div className="space-y-3 mt-10">
-        {order.length === 0 ? 
-          <p className="text-center">La orden está vacía</p>
-        :(
-          order.map( item => (
+        {order.map( item => (
             <div
               key={item.id}
               className="flex justify-between items-center border-t border-gray-200 py-5 last-of-type:border-b"
@@ -34,8 +31,7 @@ export default function OrderContents({order, removeItem} : OrderContentsProps) 
                 X
               </button>
             </div>
-          ))
-        )}
+          ))}
       </div>
     </div>
   )
